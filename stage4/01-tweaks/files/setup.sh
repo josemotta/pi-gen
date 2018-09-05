eae6b5a5152c02c045d080f720082d8278d68a2f
Jose Motta <josemotta@bampli.com>
2018-09-04 21:30:27 -0400

    update setup

--- stage4/01-tweaks/files/setup.sh
+++ stage4/01-tweaks/files/setup.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 set -e
 
+DUCKDNS_DOMAIN="canoas.duckdns.org"
+DUCKDNS_TOKEN="8ce54352-8105-46e6-a82b-1317812fd6ca"
+DUCKDNS_SSL="y"
+DOCKER_COMPOSE="/home/pi/IoT.Home.Thing/home/Docker/docker-compose"
+
 # Install dependencies
 # - hassbian scripts
 #     samba:   file server
@@ -8,18 +13,24 @@ set -e
 # - docker
 
 # Hassbian
-DUCKDNS_DOMAIN="canoas.duckdns.org"
-DUCKDNS_TOKEN="8ce54352-8105-46e6-a82b-1317812fd6ca"
-DUCKDNS_SSL="y"
-
 hassbian-config install samba
 hassbian-config install duckdns << EOF
-$DUCKDNS_DOMAIN
-$DUCKDNS_TOKEN
-$DUCKDNS_SSL
+canoas.duckdns.org
+8ce54352-8105-46e6-a82b-1317812fd6ca
+y
 EOF
 
 # Docker
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
+#groupadd docker
+usermod -aG docker pi
+
+#Docker-compose
+cp $DOCKER_COMPOSE /usr/local/bin
+chown root:root /usr/local/bin/docker-compose
+chmod 0755 /usr/local/bin/docker-compose
+
+#SSH
+ssh-keygen -t rsa -b 4096 -C "josemotta@bampli.com" -q -N "" -f /home/pi/.ssh/id_rsa
 
