ced7346ec28d94a8b91a6c45d029c20bb372c17d
Jose Motta <josemotta@bampli.com>
2018-09-06 14:14:33 -0400

    add clone IoT.Home.Pi

--- stage4/01-tweaks/files/setup.sh
+++ stage4/01-tweaks/files/setup.sh
@@ -1,24 +1,28 @@
 #!/bin/sh
 set -e
 
-DUCKDNS_DOMAIN="canoas.duckdns.org"
-DUCKDNS_TOKEN="8ce54352-8105-46e6-a82b-1317812fd6ca"
-DUCKDNS_SSL="y"
-DOCKER_COMPOSE="/home/pi/IoT.Home.Thing/home/Docker/docker-compose"
+# Home Setup
+#DUCKDNS_DOMAIN="canoas.duckdns.org"
+#DUCKDNS_TOKEN="8ce54352-8105-46e6-a82b-1317812fd6ca"
+#DUCKDNS_SSL="y"
+REPO_HOME="https://github.com/josemotta/IoT.Home.Pi.git"
+HOME="/home/pi/IoT.Home.Pi/"
+DOCKER_COMPOSE="$HOME/home/Docker/docker-compose"
+KEY_USER="josemotta@bampli.com"
+KEY_FILE="/home/pi/.ssh/id_rsa"
 
-# Install dependencies
-# - hassbian scripts
+# Hassbian scripts
 #     samba:   file server
 #     duckdns: dynamic dns
-# - docker
-
-# Hassbian
 hassbian-config install samba
 hassbian-config install duckdns << EOF
 canoas.duckdns.org
 8ce54352-8105-46e6-a82b-1317812fd6ca
 y
 EOF
+
+# IoT.Home.Pi
+git clone $REPO_HOME $HOME
 
 # Docker
 curl -fsSL get.docker.com -o get-docker.sh
@@ -32,5 +36,5 @@ chown root:root /usr/local/bin/docker-compose
 chmod 0755 /usr/local/bin/docker-compose
 
 #SSH
-ssh-keygen -t rsa -b 4096 -C "josemotta@bampli.com" -q -N "" -f /home/pi/.ssh/id_rsa
+ssh-keygen -t rsa -b 4096 -C $KEY_USER -q -N "" -f $KEY_FILE
 
