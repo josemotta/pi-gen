8ff40d0f7552b01b7b4655f12e0d5e50cc01f588
Jose Motta <josemotta@bampli.com>
2018-09-10 18:29:26 -0400

    add patch to revert to old I2C driver

--- stage4/01-tweaks/00-run.sh
+++ stage4/01-tweaks/00-run.sh
@@ -3,6 +3,8 @@
 install -d "${ROOTFS_DIR}/var/run/lirc"
 rm -f "${ROOTFS_DIR}/etc/lirc/lircd.conf.d/devinput.lircd.conf"
 
+install -m 644 files/i2c1-bcm2708.dtbo "${ROOTFS_DIR}/boot/overlays"
+
 install -m 644 files/config.txt "${ROOTFS_DIR}/boot/config.txt"
 install -m 644 files/lirc_options.conf "${ROOTFS_DIR}/etc/lirc/lirc_options.conf"
 install -m 644 files/ir-remote.conf "${ROOTFS_DIR}/etc/modprobe.d/ir-remote.conf"
@@ -10,9 +12,9 @@ install -m 644 files/lirc24.conf "${ROOTFS_DIR}/etc/lirc/lircd.conf.d/lirc24.con
 install -m 644 files/lirc44.conf "${ROOTFS_DIR}/etc/lirc/lircd.conf.d/lirc44.conf"
 install -m 644 files/Samsung_BN59-00678A.conf "${ROOTFS_DIR}/etc/lirc/lircd.conf.d/Samsung_BN59-00678A.conf"
 install -m 644 files/AppConfig.json "${ROOTFS_DIR}/app"
+
 install -m 755 files/usb_backup.sh "${ROOTFS_DIR}/home/homeassistant/usb_backup.sh"
 install -m 755 files/setup.sh "${ROOTFS_DIR}/home/pi/setup.sh"
 
 rm -f "${ROOTFS_DIR}/etc/default/keyboard"
 install -m 644 files/keyboard "${ROOTFS_DIR}/etc/default/keyboard"
-
