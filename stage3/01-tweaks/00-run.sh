32af0a1a6a5212ff9ec9bc62359be284f90051d8
Jose Motta <josemotta@bampli.com>
2018-09-24 16:17:40 -0400

    install HA via docker

--- stage3/01-tweaks/00-run.sh
+++ stage3/01-tweaks/00-run.sh
@@ -1,33 +1,33 @@
 #!/bin/bash -e
 
-on_chroot << EOF
-groupadd -f -r -g 1001 homeassistant
-useradd -u 1001 -g 1001 -rm homeassistant
-EOF
+#on_chroot << EOF
+#groupadd -f -r -g 1001 homeassistant
+#useradd -u 1001 -g 1001 -rm homeassistant
+#EOF
 
-install -v -o 1001 -g 1001 -d ${ROOTFS_DIR}/srv/homeassistant
+#install -v -o 1001 -g 1001 -d ${ROOTFS_DIR}/srv/homeassistant
 
 # Download latest Hassbian-scripts package
-cd /tmp
-curl https://api.github.com/repos/home-assistant/hassbian-scripts/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \" | wget -qi -
-HASSBIAN_PACKAGE=$(ls /tmp| grep 'hassbian*')
-install -v -m 600 /tmp/$HASSBIAN_PACKAGE ${ROOTFS_DIR}/srv/homeassistant/
-rm $HASSBIAN_PACKAGE
+#cd /tmp
+#curl https://api.github.com/repos/home-assistant/hassbian-scripts/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 | tr -d \" | wget -qi -
+#HASSBIAN_PACKAGE=$(ls /tmp| grep 'hassbian*')
+#install -v -m 600 /tmp/$HASSBIAN_PACKAGE ${ROOTFS_DIR}/srv/homeassistant/
+#rm $HASSBIAN_PACKAGE
 
-on_chroot << EOF
-dpkg -i /srv/homeassistant/*.deb
-EOF
+#on_chroot << EOF
+#dpkg -i /srv/homeassistant/*.deb
+#EOF
 
-on_chroot << EOF
-systemctl enable install_homeassistant
-EOF
+#on_chroot << EOF
+#systemctl enable install_homeassistant
+#EOF
 
-on_chroot << \EOF
-for GRP in dialout gpio i2c input netdev spi video; do
-        adduser homeassistant $GRP
-done
-for GRP in homeassistant; do
-  adduser pi $GRP
-done
-EOF
+#on_chroot << \EOF
+#for GRP in dialout gpio i2c input netdev spi video; do
+#        adduser homeassistant $GRP
+#done
+#for GRP in homeassistant; do
+#  adduser pi $GRP
+#done
+#EOF
 
--- stage4/01-tweaks/files/setup.sh
+++ stage4/01-tweaks/files/setup.sh
@@ -2,9 +2,6 @@
 set -e
 
 # Home Setup
-#DUCKDNS_DOMAIN="canoas.duckdns.org"
-#DUCKDNS_TOKEN="8ce54352-8105-46e6-a82b-1317812fd6ca"
-#DUCKDNS_SSL="y"
 REPO_HOME="https://github.com/josemotta/IoT.Home.Pi.git"
 HOME="/home/pi/IoT.Home.Pi/"
 DOCKER_COMPOSE="$HOME/home/Docker/docker-compose"
@@ -12,8 +9,11 @@ KEY_USER="josemotta@bampli.com"
 KEY_FILE="/home/pi/.ssh/id_rsa"
 
 # Hassbian scripts
-#     samba:   file server
-#     duckdns: dynamic dns
+#   samba:   file server
+#   duckdns: dynamic dns
+# DUCKDNS_DOMAIN="canoas.duckdns.org"
+# DUCKDNS_TOKEN="8ce54352-8105-46e6-a82b-1317812fd6ca"
+# DUCKDNS_SSL="y"
 hassbian-config install samba
 hassbian-config install duckdns << EOF
 canoas.duckdns.org
@@ -27,14 +27,14 @@ git clone $REPO_HOME $HOME
 # Docker
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
-#groupadd docker
+# groupadd docker
 usermod -aG docker pi
 
-#Docker-compose
+# Docker-compose
 cp $DOCKER_COMPOSE /usr/local/bin
 chown root:root /usr/local/bin/docker-compose
 chmod 0755 /usr/local/bin/docker-compose
 
-#SSH
+# SSH
 ssh-keygen -t rsa -b 4096 -C $KEY_USER -q -N "" -f $KEY_FILE
 
