14b37a4f31d74a02b9058965fbd9ece2be7847f4
Jose Motta <josemotta@bampli.com>
2018-10-02 16:08:20 -0400

    add samba server to setup

--- stage4/01-tweaks/files/setup.sh
+++ stage4/01-tweaks/files/setup.sh
@@ -14,12 +14,12 @@ KEY_FILE="/home/pi/.ssh/id_rsa"
 # DUCKDNS_DOMAIN="canoas.duckdns.org"
 # DUCKDNS_TOKEN="8ce54352-8105-46e6-a82b-1317812fd6ca"
 # DUCKDNS_SSL="y"
-hassbian-config install samba
-hassbian-config install duckdns << EOF
-canoas.duckdns.org
-8ce54352-8105-46e6-a82b-1317812fd6ca
-y
-EOF
+#hassbian-config install samba
+#hassbian-config install duckdns << EOF
+#canoas.duckdns.org
+#8ce54352-8105-46e6-a82b-1317812fd6ca
+#y
+#EOF
 
 # IoT.Home.Pi
 git clone $REPO_HOME $HOME
@@ -35,6 +35,24 @@ cp $DOCKER_COMPOSE /usr/local/bin
 chown root:root /usr/local/bin/docker-compose
 chmod 0755 /usr/local/bin/docker-compose
 
+# Samba server only without client
+sudo apt-get install -y samba samba-common-bin
+# Enable this option to install server AND client
+#sudo apt-get install -y samba samba-common-bin smbclient cifs-utils
+
+# Senha inicial do "config"
+sudo smbpasswd -a pi elefante
+
+cat << EOF >> /etc/samba/smb.conf
+[config]
+    path = /home/pi/config
+    available = yes
+    valid users = pi
+    read only = no
+    browsable = yes
+    public = yes
+    writable = yes
+EOF
+
 # SSH
 ssh-keygen -t rsa -b 4096 -C $KEY_USER -q -N "" -f $KEY_FILE
-
