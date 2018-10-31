b73c27313d9058962ac2b612cc301c16e45aa9d5
Jose Motta <josemotta@bampli.com>
2018-10-31 18:44:09 -0300

    adjusted to hassio image disk & install Lirc

--- stage4/01-tweaks/00-run.sh
+++ stage4/01-tweaks/00-run.sh
@@ -7,13 +7,11 @@ install -m 644 files/i2c1-bcm2708.dtbo "${ROOTFS_DIR}/boot/overlays"
 
 install -m 644 files/config.txt "${ROOTFS_DIR}/boot/config.txt"
 install -m 644 files/lirc_options.conf "${ROOTFS_DIR}/etc/lirc/lirc_options.conf"
-install -m 644 files/ir-remote.conf "${ROOTFS_DIR}/etc/modprobe.d/ir-remote.conf"
 install -m 644 files/lirc24.conf "${ROOTFS_DIR}/etc/lirc/lircd.conf.d/lirc24.conf"
 install -m 644 files/lirc44.conf "${ROOTFS_DIR}/etc/lirc/lircd.conf.d/lirc44.conf"
 install -m 644 files/Samsung_BN59-00678A.conf "${ROOTFS_DIR}/etc/lirc/lircd.conf.d/Samsung_BN59-00678A.conf"
-install -m 644 files/AppConfig.json "${ROOTFS_DIR}/app"
+install -m 644 files/AppConfig.json "${ROOTFS_DIR}/home/pi/AppConfig.json"
 
-install -m 755 files/usb_backup.sh "${ROOTFS_DIR}/home/pi/usb_backup.sh"
 install -m 755 files/setup.sh "${ROOTFS_DIR}/home/pi/setup.sh"
 
 rm -f "${ROOTFS_DIR}/etc/default/keyboard"
